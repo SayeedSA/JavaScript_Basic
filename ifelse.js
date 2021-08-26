9b9cc372d52c90024abb8a1a23ddff559f0fac60
Sayeed ahmed <39121672+sayeedahmedsabbir@users.noreply.github.com>
2021-08-27 00:27:24 +0600

    new file

---
+++ ifelse.js
@@ -0,0 +1,29 @@
+//To find the largest of five numbers, create a JavaScript conditional statement. Print the result. ( only use conditional statement)
+var a = -5;
+var b = -6;
+var c = -2;
+var d = -1;
+var e = 0;
+
+if(a>b && a>c && a>d && a>e)
+    {
+        console.log(a);
+    }
+else if(b>a && b>c && b>d && b>e)
+    {
+        console.log(b);
+    }
+else if(c>a && c>b && c>d && c>e)
+    {
+        console.log(c);
+    }
+else if(d>a && d>b && d>c && d>e)
+    {
+        console.log(d);
+    }
+else
+    {
+        console.log(e);
+    }
+
+    
