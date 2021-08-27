5d23030a4f11681d9871fa3d756885197d28125f
Sayeed ahmed <39121672+sayeedahmedsabbir@users.noreply.github.com>
2021-08-28 02:45:09 +0600

    new

---
+++ slice.js
@@ -0,0 +1,6 @@
+var num= [1,2,3,4,5,6]
+console.log(num)
+
+// a shallow copy from main array !! it's a sub array
+var newNum = num.slice(2)
+console.log(newNum)
---
+++ splice.js
@@ -0,0 +1,5 @@
+var num= [1,2,3,4,5,6]
+// replace value from specific index
+//num.splice(2,1,10)
+num.splice(5,1,10)
+console.log(num)
